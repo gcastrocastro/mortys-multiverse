ce56cacf3165e2eaef33fe6a52181150d6e5e51d
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-12 19:03:25 -0400

    wip: adds authpage and navbar component, deletes app.css styles

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from '../../Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">  
+      <NavBar/>
       <h1>HomePage</h1>
     </div>
   );
