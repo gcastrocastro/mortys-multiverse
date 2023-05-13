f288cdd4ce1654ed8186e1667a50adc95ba7fd9e
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-12 21:38:07 -0400

    wip: allcharacter page styles and links

--- src/Pages/AllCharacters/AllCharacters.js
+++ src/Pages/AllCharacters/AllCharacters.js
@@ -1,6 +1,8 @@
 import {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import './AllCharacters.css';
 
-export default function AllCharacters({apiUrl}) {
+export default function AllCharacters() {
     const [characters, setCharacters] = useState([]);
     const apiUrlChars = 'https://rickandmortyapi.com/api/character';
 
@@ -26,19 +28,20 @@ export default function AllCharacters({apiUrl}) {
     }, []);
 
     return(
-        <section>
-            <h1>AllCharactersPage</h1>
+        <section className='character-container'>
             {characters.map(char => {
                 return(
-                    <div className='character-card'>
-                        <img src={char.image} alt={char.name}/>
-                        <div className='character-info'>
-                            <h3>Name: {char.name}</h3>
-                            <h4>Species: {char.species}</h4>
-                            <h4>Origin: {char.origin.name}</h4>
-                            <h4>Most Recent Location: {char.location.name}</h4>
+                    <Link to={`/characters/${char.id}`} key={char.id}>
+                        <div className='character-card'>
+                            <img src={char.image} alt={char.name}/>
+                            <div className='character-info'>
+                                <h2>{char.name}</h2>
+                                <h4>Species: {char.species}</h4>
+                                <h4>Origin: {char.origin.name}</h4>
+                                <h4>Most Recent Location: {char.location.name}</h4>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 )
             })}
         </section>
