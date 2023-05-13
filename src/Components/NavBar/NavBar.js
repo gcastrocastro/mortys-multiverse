5369fb6a2273db6c2bee6e23e2d03a86357a9360
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-12 22:54:11 -0400

    wip: navbar styling

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,9 +1,10 @@
 import {Link} from 'react-router-dom';
+import './NavBar.css';
 
 export default function NavBar(){
     return(
         <nav>
-            <Link to ="/characters"> Explore all characters </Link>
+            <Link to ="/characters"> Explore All Characters </Link>
             &nbsp; | &nbsp;
             <Link to ="/favorites"> Your Favorites </Link>
         </nav>
