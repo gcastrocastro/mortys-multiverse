2d15bcf8722d9217542a25c2ac4f110e90ba964c
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-13 00:05:49 -0400

    wip: nav bar link styles

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,7 +5,6 @@ export default function NavBar(){
     return(
         <nav>
             <Link to ="/characters"> Explore All Characters </Link>
-            &nbsp; | &nbsp;
             <Link to ="/favorites"> Your Favorites </Link>
         </nav>
     )
