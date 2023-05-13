10d0e9251d8817d96d30576195b9c2a97be8e0c3
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-12 23:38:29 -0400

    wip: adds favorite heart icon

--- src/Pages/AllCharacters/AllCharacters.js
+++ src/Pages/AllCharacters/AllCharacters.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import './AllCharacters.css';
+import 'boxicons';
 
 export default function AllCharacters() {
     const [characters, setCharacters] = useState([]);
@@ -31,17 +32,22 @@ export default function AllCharacters() {
         <section className='character-container'>
             {characters.map(char => {
                 return(
-                    <Link to={`/characters/${char.id}`} key={char.id}>
-                        <div className='character-card'>
-                            <img src={char.image} alt={char.name}/>
-                            <div className='character-info'>
+                    <div className='character-card'>
+                        <div className="favorite-icon">
+                            <Link to={`/favorites`}>
+                              <box-icon name='heart'></box-icon>
+                            </Link>
+                        </div>
+                        <img src={char.image} alt={char.name}/>
+                        <div className='character-info'>
+                            <Link to={`/characters/${char.id}`} key={char.id}>
                                 <h2>{char.name}</h2>
                                 <h4>Species: {char.species}</h4>
                                 <h4>Origin: {char.origin.name}</h4>
                                 <h4>Most Recent Location: {char.location.name}</h4>
-                            </div>
+                            </Link>
                         </div>
-                    </Link>
+                    </div>
                 )
             })}
         </section>
