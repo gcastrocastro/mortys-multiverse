d3052e4234946be06c11395d223f097273e1c91f
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-16 14:59:02 -0400

    wip: add status bar to charactercard component + styling

--- src/Components/CharacterCard/CharacterCard.js
+++ src/Components/CharacterCard/CharacterCard.js
@@ -1,9 +1,24 @@
 import {Link} from 'react-router-dom';
-import HeartIcon from "../HeartIcon/HeartIcon"
+import HeartIcon from "../HeartIcon/HeartIcon";
+import './CharacterCard.css';
 
 export default function CharacterCard({user, char, favorites, setFavorites}) {
+    const getBackgroundColor = () => {
+        if (char.status === 'Alive') {
+          return 'green';
+        } else if (char.status === 'Dead') {
+          return 'red';
+        } else {
+          return 'gray';
+        }
+      };
+
     return (
         <div className='character-card' key={char.id}>
+            <span className='character-status' style={{ backgroundColor: getBackgroundColor() }}>
+                {/* <span status_icon></span> */}
+                <span>{char.status}</span>
+            </span>
             <div className="favorite-icon">
                 <HeartIcon user={user} id={char.id} favorites={favorites} setFavorites={setFavorites}/>
             </div>
