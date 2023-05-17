40399e22c4f357a04c6bba11bff56b77bacf5cd0
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-17 12:25:43 -0400

    fix: trying to push again after deleting songs

--- src/Components/AudioPlayer/songList.js
+++ src/Components/AudioPlayer/songList.js
@@ -4,14 +4,6 @@ const songList = [
         url: require('../../songs/Goodbye_Moonmen.mp3')
       },
       {
-        title: 'Evil Morty Theme',
-        url: require('../../songs/For_The_Damaged_Coda.mp3')
-      },
-      {
-        title: 'Terryfolds',
-        url: require('../../songs/Terryfolds.mp3')
-      },
-      {
         title: 'Get Schwifty',
         url: require('../../songs/Get_Schwifty_.mp3')
       }
