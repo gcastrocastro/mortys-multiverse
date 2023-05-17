0dc6e3e8895791c791af42c92baff068ab240ea0
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-17 17:23:50 -0400

    fix: styling and add dayssince to homepage

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -2,11 +2,20 @@ import ParticleBackground from "../../Components/Particles/Particles"
 import './HomePage.css';
 
 export default function HomePage() {
+
+    const daysSinceLastEpisode = () =>  {
+        const lastEpisodeDate = new Date('2022-10-03');
+        let today = new Date();
+        const timeDiff = Math.abs(today.getTime() - lastEpisodeDate.getTime());
+        var daysPassed = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        return daysPassed;
+      }
+
     return(
         <>
         <ParticleBackground/>
         <div className="homepage-container">
-            <h1> Days since Last Rick and Morty Episode Launched: </h1>
+            <h1> Days since Last Rick and Morty Episode Launched: {daysSinceLastEpisode()}</h1>
         </div>
         </>
     )
