c80396309672d351bc559e9c66812a83abb925e5
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-17 13:19:06 -0400

    fix: made username link to homepage

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -11,7 +11,7 @@ export default function NavBar({user, setUser}){
 
     return(
         <nav>
-            {user && <h2>&nbsp; Welcome, {user.name}</h2>}
+            {user && <Link to="/"><h2>&nbsp; Welcome, {user.name}</h2></Link>}
             <Link to ="/characters"> Explore All Characters </Link>
             <Link to ="/favorites"> Your Favorites </Link>
             <Link to="/" onClick={handleLogout}>Log Out</Link>
