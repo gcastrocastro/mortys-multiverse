782eb2130af5a490ea99b0058c35c81ef23e56de
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-17 19:54:19 -0400

    wip: homepage content and styling

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -33,23 +33,24 @@ export default function HomePage({user}) {
     return(
         <>
         <ParticleBackground/>
-            <div
-              className="robot-container"
-              onMouseEnter={handleHover}
-              onMouseLeave={handleMouseLeave}
-              onClick={handleClick}
-            >
-              <h3>{hovered ? `You show ${user.name} their current stats!` : 'What is my purpose?'}</h3>
-              <img src={hovered ? 'https://i.imgur.com/TxHmKCo.png' : 'https://i.imgur.com/mrik8SS.png'} alt="Image" />
-            </div>
+        <h1 className="home-title"> Morty's Multiverse</h1>
+        <div
+          className="robot-container"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleMouseLeave}
+          onClick={handleClick}
+        >
+          <h3>{hovered ? `You show ${user.name} their current stats!` : 'What is my purpose?'}</h3>
+          <img src={hovered ? 'https://i.imgur.com/TxHmKCo.png' : 'https://i.imgur.com/mrik8SS.png'} alt="Image" />
+        </div>
     
-          {clicked && (
-            <div className="stats" onClick={handleDivClick}>
-              <h2> Days since Last Rick and Morty Episode Launched: <br></br> {daysSinceLastEpisode()}</h2>  
-              <h2>Number of Character's {user.name} has Favorited: <br></br> {user.favorites.length}</h2>
-              <h2>Number of Character's Included so far: <br></br> </h2>
-              <h2>Number of Episodes Included so far: <br></br> </h2>
-            </div>
+        {clicked && (
+          <div className="stats" onClick={handleDivClick}>
+            <h2> Days since Last Rick and Morty Episode Launched: <br></br> {daysSinceLastEpisode()}</h2>  
+            <h2>Number of Character's {user.name} has Favorited: <br></br> {user.favorites.length}</h2>
+            <h2>Number of Character's Included so far: <br></br> </h2>
+            <h2>Number of Episodes Included so far: <br></br> </h2>
+          </div>
       )}
         </>
     )
