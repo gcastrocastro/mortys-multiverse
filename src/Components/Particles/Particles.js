7184530a3baf0aaefcbdd929715d995e68806075
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-16 22:02:44 -0400

    fix: particles are now working

--- src/Components/Particles/Particles.js
+++ src/Components/Particles/Particles.js
@@ -1,10 +1,20 @@
+import { useCallback } from "react";
+import { loadFull } from "tsparticles";
 import Particles from "react-tsparticles";
 import ParticlesConfig from "./Particles-config";
 
-const ParticleBackground = () => {
+export default function ParticleBackground() {
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
     return(
-        <Particles id='tsparticles' options={ParticlesConfig}/>
+        <Particles id='tsparticles' init={particlesInit} loaded={particlesLoaded} options={ParticlesConfig}/>
     )
 }
-
-export default ParticleBackground;
