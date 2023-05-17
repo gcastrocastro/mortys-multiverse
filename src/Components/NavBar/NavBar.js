63977fcde47a7f2e2ac7f64597c00770ee8da30e
gcastrocastro <gcastrocastro.25@gmail.com>
2023-05-17 13:59:20 -0400

    fix: nav styling

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -11,7 +11,7 @@ export default function NavBar({user, setUser}){
 
     return(
         <nav>
-            {user && <Link to="/"><h2>&nbsp; Welcome, {user.name}</h2></Link>}
+            {user && <Link to="/"><h3>&nbsp; Welcome, {user.name}</h3></Link>}
             <Link to ="/characters"> Explore All Characters </Link>
             <Link to ="/episodes"> Explore All Episodes </Link>
             <Link to ="/favorites"> Your Favorites </Link>
